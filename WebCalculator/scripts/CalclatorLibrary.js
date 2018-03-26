da672d053dcce1974e5beb7c9540e312a6bdad2b
Zer0cul <16000917+Zer0cul@users.noreply.github.com>
2018-03-26 15:25:59 +0300

    Add buttons click test and Plus function tests

--- WebCalculator/scripts/CalclatorLibrary.js
+++ WebCalculator/scripts/CalclatorLibrary.js
@@ -1,11 +1,22 @@
 ﻿var txtInput;
+var txtResult;
 
 function initialize() {
+
+    //document.getElementById('btn5').addEventListener('click', numberClick, false);
+    for (var i = 0 ; i < 10 ; i++) {
+        document.getElementById('btn' + i).addEventListener('click', numberClick, false);
+    }
     txtInput = document.getElementById('txtInput');
-    document.getElementById('btn5').addEventListener('click', numberClick, false);
+    txtResult = document.getElementById('txtResult');
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
 }
 
 function numberClick() {
     txtInput.value = txtInput.value == '0' ?
         this.innerText : txtInput.value + this.innerText;
+}
+
+function plusClick() {
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
 }
