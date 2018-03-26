caf05d60d34635797f45d069210190d4bb3fefb7
Zer0cul <16000917+Zer0cul@users.noreply.github.com>
2018-03-26 15:02:27 +0300

    Add WebCalculator project
    This includes steps for practice excercise from 1 to 15 inclusive.

---
+++ WebCalculator/scripts/CalclatorLibrary.js
@@ -0,0 +1,11 @@
+﻿var txtInput;
+
+function initialize() {
+    txtInput = document.getElementById('txtInput');
+    document.getElementById('btn5').addEventListener('click', numberClick, false);
+}
+
+function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+        this.innerText : txtInput.value + this.innerText;
+}
