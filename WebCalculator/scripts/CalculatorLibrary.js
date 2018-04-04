6976c82cbd102da1446eda260125274d79903e59
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-04-04 13:51:03 +0300

    Change fix for button realisatoin
    missing # sign in selector produces  output to txtInput as 'undefined'

--- WebCalculator/scripts/CalculatorLibrary.js
+++ WebCalculator/scripts/CalculatorLibrary.js
@@ -7,17 +7,11 @@
     ns.initialize = function () {    
         var calculator = new ns.Calculator();
 
-        //document.getElementById('btn5').addEventListener('click', numberClick, false);
-        //for (var i = 0; i < 10; i++) {
-        //    document.getElementById('btn' + i).addEventListener('click', numberClick, false);
-        //}
-        //txtInput = document.getElementById('txtInput');
-        //txtResult = document.getElementById('txtResult');
         $('button[id^="btnNumber"]').on('click', calculator.numberClick);
-        $('btnPlus').on('click', calculator.plusClick);
-        $('btnMinus').on('click', calculator.minusClick);
-        $('btnClearEntry').on('click', calculator.clearEntry);
-        $('btnClear').on('click', calculator.clear);
+        $('#btnPlus').on('click', calculator.plusClick);
+        $('#btnMinus').on('click', calculator.minusClick);
+        $('#btnClearEntry').on('click', calculator.clearEntry);
+        $('#btnClear').on('click', calculator.clear);
         calculator.clear();
     }
     
@@ -26,19 +20,20 @@
         }
 
         Calculator.prototype.numberClick = function numberClick() {
-            $('#txtInput').val($('#txtInput').val() == '0' ? $(this).text() : $('txtInput').val() + $(this).text());
+            $('#txtInput').val($('#txtInput').val() == '0' ?
+                $(this).text() : $('#txtInput').val() + $(this).text());
         }
 
 
         Calculator.prototype.plusClick = function plusClick() {
             //txtResult.value = Number(txtResult.value) + Number(txtInput.value);
-            $('#txtResult').val(Number($('#txtResult').val) + Number($('#txtInput').val()));
+            $('#txtResult').val(Number($('#txtResult').val()) + Number($('#txtInput').val()));
             
             Calculator.prototype.clearEntry();
         }
 
         Calculator.prototype.minusClick = function minusClick() {
-            $('#txtResult').val(Number($('#txtResult').val) - Number($('#txtInput').val()));
+            $('#txtResult').val(Number($('#txtResult').val()) - Number($('#txtInput').val()));
             //txtResult.value = Number(txtResult.value) - Number(txtInput.value);
             Calculator.prototype.clearEntry();
         }
--- WebCalculator/scripts/tests.js
+++ WebCalculator/scripts/tests.js
@@ -10,8 +10,8 @@ test("Initialize Test", function () {
 
 test("Btn5 Click Test", function () {
     expect(1);
-    var btn = document.getElementById('btn5');
-    QUnit.triggerEvent(btn, "click");
+    var btn = $('#btnNumber5').triggerHandler('click');
+    //QUnit.triggerEvent(btn, "click");
     var result = txtInput.value;
     var expected = '5';
     equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
@@ -21,28 +21,39 @@ test("Button Click Test", function () {
     var buttonQuantity = 10;
     expect(buttonQuantity * 2);
     for (var i = 0; i < buttonQuantity; i++) {
-        var btn = document.getElementById('btn' + i);
-        QUnit.triggerEvent(btn, "click");
-        var result = txtInput.value[txtInput.value.length - 1];
+        $('#btnNumber' + i).triggerHandler('click');
+        var result = $('#txtInput').val()[$('#txtInput').val().length -1];//txtInput.value[txtInput.value.length - 1];
         var expected = String(i);
         equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
         var expectedLength = i < 2 ? 1 : i;
-        equal(txtInput.value.length, expectedLength, 'Expected string length: ' + expectedLength +
-            ' Actual value: ' + txtInput.value.length);
+        //var expectedLength = i + 1;
+        // after i=2 always return 10
+        var actual = Number($('#txtInput').val().length);
+        equal(actual, expectedLength, 'Expected string length: ' + expectedLength + '  Actual value: ' + actual);
     }
+});
+test("Two number Button Click Test", function () {
+    var buttonQuantity = 10;
+    expect(1);
+    var i = Number(1);
+    $('#btnNumber' + i).triggerHandler('click');
+    i++;
+    $('#btnNumber' + i).triggerHandler('click');
+    var expectedLength = 2;
+    var actual = Number($('#txtInput').val().length);
+    equal(actual, expectedLength, 'Expected string length: ' + expectedLength + '  Actual value: ' + actual);
+
 });
 
 test("Add Test", function () {
     expect(2);
-    txtInput.value = '10';
-    txtResult.value = '20';
-    var btnPlus = document.getElementById('btnPlus');
-    QUnit.triggerEvent(btnPlus, "click");
+    $('#txtInput').val('10');
+    $('#txtResult').val('20');
+    $('#btnPlus').triggerHandler('click');
     var expected = '30';
-    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    equal($('#txtResult').val(), expected, 'Expected value: ' + expected + '  Actual value: ' + $('#txtResult').val());
     expected = '0';
-    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
-
+    equal($('#txtInput').val(), expected, 'Expected value: ' + expected + '  Actual value: ' + $('#txtInput').val());
 });
 
 test("Subtrack Test", function () {
