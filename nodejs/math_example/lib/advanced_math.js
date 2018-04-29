c7d5f33858fc74d217922fa5770150d73efc56b1
Zer0cul <16000917+Zer0cul@users.noreply.github.com>
2018-04-29 15:06:17 +0300

    Add nodejs module

---
+++ nodejs/math_example/lib/advanced_math.js
@@ -0,0 +1,37 @@
+﻿//Reading Chapter8
+
+var call_counter = require('./call_counter');
+
+function multiply(x,y) {
+    call_counter();
+    return x * y;
+}
+
+function divide(x, y) {
+    call_counter();
+    return x / y;
+}
+
+function fibo(count) {
+    call_counter();
+    return private_Fibo(count);
+}
+
+function private_Fibo(count, counter, first, second) {
+    if (count == 0) return 0;
+    if (conter == undefined) {
+        counter = 1;
+        first = 1;
+        second = 2;
+    }
+    var result = first + second;
+    if (counter == count) return result;
+    private_Fibo(count, ++counter, second, result);
+    return result;
+}
+
+module.exports = {
+    multiplication: multiply,
+    division: divide,
+    fibonacci: fibo
+}
