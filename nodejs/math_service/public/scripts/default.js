ff6d4e9db6b45b3baf1b17e55e599ef73154f942
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-04-30 18:16:39 +0300

    Add Syncronious call of the math_service

---
+++ nodejs/math_service/public/scripts/default.js
@@ -0,0 +1,15 @@
+﻿//Perform AJAX query
+$(document).ready(function () {
+    $('#btnAdd').on('click',addNumbers)
+});
+
+function addNumbers() {
+    var x = document.getElementById('x').value;
+    var y = document.getElementById('y').value;
+    var result = document.getElementById('result');
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "addition?x=" + x + "&y=" + y, false);
+    xmlhttp.send();
+    var jsonObject = JSON.parse(xmlhttp.response);
+    result.innerHTML = jsonObject.result;
+}
