f62f76704bcfb8b50b42255e0fb7afd1dd219b4a
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-04-30 15:14:35 +0300

    Add HelloExpress application server

---
+++ nodejs/HelloExpress/app.js
@@ -0,0 +1,22 @@
+﻿//Application that use express package to deliver web server functionality
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, response) {
+    response.send('Hello world');
+});
+
+app.get('/SubmitHello', function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('Hello ' + request.query.userName + '!<br />');
+    response.end('Have a great day!');
+    console.log('Handled request from ' + request.query.userName);
+});
+
+
+var port = 8080;
+app.listen(port);
+console.log('Listen on port:' + port);
+
