6832e6defa9854c07cdcbc81b96043930644511a
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-04-30 18:29:07 +0300

    Add asyncronious code on page but it does not working

--- nodejs/math_service/public/scripts/default.js
+++ nodejs/math_service/public/scripts/default.js
@@ -8,8 +8,32 @@ function addNumbers() {
     var y = document.getElementById('y').value;
     var result = document.getElementById('result');
     var xmlhttp = new XMLHttpRequest();
+    //0 Uninitialized The open method has not been called yet.
+    //1 Loading The send method has not been called yet.
+    //2 Loaded The send method has been called; headers and status are available.
+    //3 Interactive Downloading; the response properties hold the partial data.
+    //4 Completed All operations are finished. 
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var jsonObject = JSON.parse(xmlhttp.response);
+            result.innerHTML = jsonObject.result;
+        };
+    };
+    xmlhttp.addEventListener("progress", updateProgress, false);
+    xmlhttp.addEventListener("error", failed, false);
+    xmlhttp.addEventListener("abort", canceled, false); 
     xmlhttp.open("GET", "addition?x=" + x + "&y=" + y, false);
     xmlhttp.send();
-    var jsonObject = JSON.parse(xmlhttp.response);
-    result.innerHTML = jsonObject.result;
 }
+
+function updateProgress(evt) {
+    if (evt.lengthComputable) {
+        var percentComplete = evt.loaded / evt.total
+        //display percentComplete
+    } else {
+        // Need total size to compute progress
+    }
+}
+
+function transferFailed(evt) { alert("An error occurred"); }
+function canceled(evt) { alert("canceled by the user"); }
