174f7e5eb790f36075fd0db08d8a2445ff8a4ec9
Zer0cul <16000917+Zer0cul@users.noreply.github.com>
2018-04-30 21:54:56 +0300

    Add AJAX jQuery version of the service request

--- nodejs/math_service/public/scripts/default.js
+++ nodejs/math_service/public/scripts/default.js
@@ -4,26 +4,19 @@ $(document).ready(function () {
 });
 
 function addNumbers() {
-    var x = document.getElementById('x').value;
-    var y = document.getElementById('y').value;
-    var result = document.getElementById('result');
-    var xmlhttp = new XMLHttpRequest();
-    //0 Uninitialized The open method has not been called yet.
-    //1 Loading The send method has not been called yet.
-    //2 Loaded The send method has been called; headers and status are available.
-    //3 Interactive Downloading; the response properties hold the partial data.
-    //4 Completed All operations are finished. 
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var jsonObject = JSON.parse(xmlhttp.response);
-            result.innerHTML = jsonObject.result;
-        }
-    }
-    xmlhttp.addEventListener("progress", updateProgress, false);
-    xmlhttp.addEventListener("error", transferFailed, false);
-    xmlhttp.addEventListener("abort", canceled, false); 
-    xmlhttp.open("GET", "addition?x=" + x + "&y=" + y, false);
-    xmlhttp.send();
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var data = { "x": x, "y": y };
+    $.ajax({
+        url: '/addition',
+        data: data,
+        type: 'GET',
+        cache: false,
+        dataType: 'json',
+        success: function (data) {
+            $('#result').html(data.result);
+        }       
+    });
 }
 
 function updateProgress(evt) {
