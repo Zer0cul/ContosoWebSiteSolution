5f3e2e6a9cceec169024ebc1b504d80d45285c97
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-04-30 12:58:53 +0300

    Add missing main.js file

---
+++ nodejs/z9math_example/bin/main.js
@@ -0,0 +1,13 @@
+﻿//Reading Chapter 8  pate 348
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+var simple = require(lib + '/simple_math.js');
+var advanced = require(lib + '/advanced_math.js');
+module.exports = {
+    addition: simple.addition,
+    subtraction: simple.subtraction,
+    multiplication: advanced.multiplication,
+    division: advanced.division,
+    fibonacci: advanced.fibonacci
+}
