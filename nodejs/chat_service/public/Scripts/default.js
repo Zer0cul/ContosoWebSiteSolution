d3f34bd521e3c8de1995fa0c52c53e942b81236b
Mykhaylo Rudakov <Mykhaylo.Rudakov@lanteria.net>
2018-05-04 11:50:38 +0300

    Add Chat service. Client and Server.

---
+++ nodejs/chat_service/public/Scripts/default.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="socket.io.dev.js" />
+
+var socket;
+$(document).ready(function () {
+    socket = io('http://localhost:8080');
+    socket.on('connect', addUser);
+    socket.on('updatechat', processMessage);
+    socket.on('updateusers', updateUserList);
+    $('#datasend').click(sendMessage);
+    $('#data').keypress(processEnterPress);
+
+    socket = io.connect();
+});
+
+function addUser() {
+    socket.emit('addUser', prompt("What's your name?"));
+}
+
+function processMessage(username, data) {
+    $('<b>' + username + ':</b>' + data + '<br />').insertAfter($('#conversation'));
+}
+function updateUserList(data) {
+    $('#users').empty();
+    
+    $.each(data, function (key, value) {
+        $('#users').append('<div>' + key + '</div>');
+    });
+}
+
+function sendMessage() {
+    var message = $('#data').val();
+    $('#data').val('');
+    socket.emit('sendchat', message);
+    $('#data').focus();
+}
+
+function processEnterPress(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        $(this).blur();
+        $('#datasend').focus().click();
+    }
+}
